373e3ea5dcdffa63593fdadb9a888b9c294ec7d6
Jeroen Claassens <support@favware.tech>
2025-01-04 22:15:39 +0100

    fix: properly map db columns to lower snake case

--- prisma/migrations/20250104211433_init/migration.sql
+++ prisma/migrations/20250104211433_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "interval" AS ENUM ('DAILY', 'WEEKLY', 'BIWEEKLY', 'MONTHLY', 'YEARLY');
+CREATE TYPE "interval" AS ENUM ('daily', 'weekly', 'biweekly', 'monthly', 'yearly');
 
 -- CreateEnum
 CREATE TYPE "roles" AS ENUM ('all_rounder', 'healer', 'melee_dps', 'magic_ranged_dps', 'phys_ranged_dps', 'tank', 'absence', 'bench', 'late', 'tentative');
@@ -10,9 +10,9 @@ CREATE TYPE "jobs" AS ENUM ('all_rounder', 'paladin', 'warrior', 'dragoon', 'mon
 -- CreateTable
 CREATE TABLE "event_managers" (
     "id" TEXT NOT NULL,
-    "discordId" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "discord_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "event_managers_pkey" PRIMARY KEY ("id")
 );
@@ -20,9 +20,9 @@ CREATE TABLE "event_managers" (
 -- CreateTable
 CREATE TABLE "verified_servers" (
     "id" TEXT NOT NULL,
-    "discordId" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "discord_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "verified_servers_pkey" PRIMARY KEY ("id")
 );
@@ -36,8 +36,8 @@ CREATE TABLE "events" (
     "leader" TEXT NOT NULL,
     "role_to_ping" TEXT,
     "channel_id" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "events_pkey" PRIMARY KEY ("id")
 );
@@ -48,8 +48,8 @@ CREATE TABLE "event_instances" (
     "event_id" TEXT NOT NULL,
     "message_id" TEXT,
     "date_time" TIMESTAMP(3) NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "event_instances_pkey" PRIMARY KEY ("id")
 );
@@ -59,29 +59,29 @@ CREATE TABLE "participants" (
     "id" TEXT NOT NULL,
     "role" "roles" NOT NULL,
     "job" "jobs" NOT NULL,
-    "discordId" TEXT NOT NULL,
-    "signupOrder" INTEGER NOT NULL,
+    "discord_id" TEXT NOT NULL,
+    "signup_order" INTEGER NOT NULL,
     "event_id" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "participants_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "event_managers_discordId_key" ON "event_managers"("discordId");
+CREATE UNIQUE INDEX "event_managers_discord_id_key" ON "event_managers"("discord_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "verified_servers_discordId_key" ON "verified_servers"("discordId");
+CREATE UNIQUE INDEX "verified_servers_discord_id_key" ON "verified_servers"("discord_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "event_instances_event_id_key" ON "event_instances"("event_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "participants_discordId_key" ON "participants"("discordId");
+CREATE UNIQUE INDEX "participants_discord_id_key" ON "participants"("discord_id");
 
 -- AddForeignKey
-ALTER TABLE "event_instances" ADD CONSTRAINT "event_instances_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "event_instances" ADD CONSTRAINT "event_instances_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "participants" ADD CONSTRAINT "participants_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "event_instances"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
