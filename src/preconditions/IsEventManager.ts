d323bb4a31d0ff2e7b338c96216c7c96b4694900
Jeroen Claassens <jeroen.claassens@live.nl>
2025-02-07 16:35:33 +0100

    refactor: allow owners to use commands regardless of event manager role (#12)
    * refactor: allow owners to use commands regardless of event manager role
    
    * Update IsEventManager.ts (#11)
    
    * Update IsEventManager.ts
    
    * Update IsEventManager.ts

--- src/preconditions/IsEventManager.ts
+++ src/preconditions/IsEventManager.ts
@@ -1,9 +1,15 @@
 import { BloombotEmojis } from '#lib/util/emojis';
 import { Precondition } from '@sapphire/framework';
+import { Owners } from '#root/config';
 import type { ChatInputCommandInteraction } from 'discord.js';
 
 export class UserPrecondition extends Precondition {
 	public override async chatInputRun(interaction: ChatInputCommandInteraction<'cached'>) {
+		// Check if the user ID is included in the Owners array
+		if (Owners.includes(interaction.user.id)) {
+			return this.ok();
+		}
+
 		const managers = await this.container.prisma.eventManagers.findMany({ select: { discordId: true } });
 
 		if (!managers) {
