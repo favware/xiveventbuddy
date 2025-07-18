383e12819c2c3a12119b1ef837332b628d7d4391
Jeroen Claassens <support@favware.tech>
2025-07-18 10:30:29 +0200

    refactor: cleanup formatting

--- src/listeners/error.ts
+++ src/listeners/error.ts
@@ -6,7 +6,10 @@ export class UserListener extends Listener<typeof Events.Error> {
 	}
 
 	public override async onLoad() {
-		if ((this.container.client.logger as Logger).level > LogLevel.Error) await this.unload();
+		if ((this.container.client.logger as Logger).level > LogLevel.Error) {
+			await this.unload();
+		}
+
 		return super.onLoad();
 	}
 }
