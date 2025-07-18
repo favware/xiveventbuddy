dfc71b4c5a1e0dc4c40187d64a0b05764ea62a97
Jeroen Claassens <support@favware.tech>
2025-07-18 12:46:22 +0200

    feat: add migration to drop role_to_ping column from events table

---
+++ prisma/migrations/20250718104612_remove_role_to_ping_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `role_to_ping` on the `events` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "events" DROP COLUMN "role_to_ping";
