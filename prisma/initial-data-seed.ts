3492f811cc99dcc7d7480ce0234c927ad42d8e0b
Jeroen-Claasens <Jeroen.Claasens@prorail.nl>
2026-06-23 14:09:17 +0200

    refactor: switch from tsx to unrun

--- prisma/initial-data-seed.ts
+++ prisma/initial-data-seed.ts
@@ -1,6 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import '../src/lib/types/Augments.d.ts';
 
-const prisma = new PrismaClient();
+import { PrismaPg } from '@prisma/adapter-pg';
+import { envParseString, setup } from '@skyra/env-utilities';
+import { PrismaClient } from '../src/lib/generated/prisma-client/client';
+
+setup();
+
+const adapter = new PrismaPg({
+	connectionString: envParseString('DATABASE_URL')
+});
+
+const prisma = new PrismaClient({ adapter });
 
 try {
 	await prisma.eventManagers.create({
